184fd3bab96b4f1306cc970518be631add0ab54d
Maria Luiza <mluizaguedessilva@gmail.com>
2023-11-19 16:40:37 -0300

    Create Passageiro.java

---
+++ Engenharia 1/scr/main/Passageiro.java
@@ -0,0 +1,38 @@
+package main;
+
+public class Passageiro {
+    protected String nome;
+    protected String cpf;
+    protected int codigo;
+
+    public Passageiro(String nome, String cpf, int codigo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+    
+}
